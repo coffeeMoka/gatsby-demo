9efa79bca79b7a3b12df6d6e4333cffaf5ec8b17
Takuma Nagata <takuma.fn@gmail.com>
2020-04-23 16:54:43 +0900

    Update index-page.js and Navbar.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
-  image,
   title,
   heading,
   subheading,
